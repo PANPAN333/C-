fb6d89f8dac9b0e88e4bddf22f417afbb520e20a
Panov Timofey <kirito.panov57@Gmail.com>
2022-07-20 21:32:22 +0300

    недоделанная задача вернуться позже

---
+++ Sem002_Number3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число");
+            string? number = Console.ReadLine();
+            var str = number.ToString();
+            if (99 < str && str < 1000) ;
+            {
+                int result = int.Parse(str.Remove(str.Length - 2, 1 ));
+                Console.WriteLine(result);
+            }
+            else 
+            Console.WriteLine("Неверный диапазон!");
+
+        }
+
+
+
+    }
+}
