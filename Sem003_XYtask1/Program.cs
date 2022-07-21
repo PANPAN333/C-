0c870cedfb4c8f155cc425e11e347ffd1d97aa39
Panov Timofey <kirito.panov57@Gmail.com>
2022-07-21 15:07:34 +0300

    1 ЗАДАНИЕ по работе с координатами XY

---
+++ Sem003_XYtask1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 1 Напишите программу, которая принимает на вход координаты точки(X и Y), причём X (неравен нулю) и Y (неравен нулю)
+// и выдаёт номер четверти плоскости, в которой находится эта точка.  namespace Sem003_XY
+
+namespace Sem003_XY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberX = Convert.ToInt32(Console.ReadLine());
+            int numberY = Convert.ToInt32(Console.ReadLine());
+
+            if (numberX == 0 || numberY == 0)
+            {
+                System.Console.WriteLine("We don't know");
+            }
+            else
+            {
+                if (numberX > 0)
+                {
+                    if (numberY > 0)
+                    {
+                        Console.WriteLine(1);
+                    }
+                    else
+                    {
+                        Console.WriteLine(4);
+                    }
+                }
+                else
+                {
+                    if (numberY > 0)
+                    {
+                        Console.WriteLine(2);
+                    }
+                    else
+                    {
+                        Console.WriteLine(3);
+                    }
+                }
+            }
+
+
+        }
+    }
+}
