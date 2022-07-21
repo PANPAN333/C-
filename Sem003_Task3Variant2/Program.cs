245dda36ae6014dc581b07ccce6a929358cc0572
Panov Timofey <kirito.panov57@Gmail.com>
2022-07-21 15:03:10 +0300

    Второй вариант возведения всех чисел до N в квадрат

---
+++ Sem003_Task3Variant2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Возводит все числа до N в квадрат
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Читаем число с консоли
+            int a = Convert.ToInt32(Console.ReadLine());
+            // Перебор массива с помощью цикла 
+            for (int i = 1; i <= a; i++)
+            {
+                // Вывод чисел в консоль
+                PrintSquare(i);
+            }
+        }
+        // Обьявление функции 
+        public static void PrintSquare(int n)
+        {
+            Console.WriteLine(n * n);
+        }
+    }
+}
