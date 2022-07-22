6260177d5f1caeaf86d4050392c48ad401323b21
Panov Timofey <kirito.panov57@Gmail.com>
2022-07-22 18:09:56 +0300

    работа с факториалами 1 задание

---
+++ Sem004_Task1_Factorial/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task1_Factorial
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите целое число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result = result * i;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
