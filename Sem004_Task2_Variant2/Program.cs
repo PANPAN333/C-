2e4fb68272f6ffe1e69f4f3eec3c4880f00ce5c6
Panov Timofey <kirito.panov57@Gmail.com>
2022-07-22 18:14:22 +0300

    2 вариант 2 задания с рандомными массивами

---
+++ Sem004_Task2_Variant2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и еиницами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+namespace Task2_RandomMassiv_Variant2
+{
+    class Program
+    {
+        public static void Main()
+        {
+           int[] arrayNumbers = new int[8];
+           Random(arrayNumbers);
+           PrintArray(arrayNumbers);
+
+        }
+
+        public static void Random(int[] array)
+        {
+            int count = array.Length;
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = new Random().Next(0, 2);
+            }
+        }
+        public static void PrintArray(int[] array)
+        {
+          int count = array.Length;
+          for (int i = 0; i < count; i++)
+          {
+            Console.Write($"{array[i]} ");
+          }
+        }
+    }
+}
