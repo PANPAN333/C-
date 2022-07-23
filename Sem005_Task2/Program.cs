0d802007e9677a4af7f6af8e6f0fcdcde231ba99
Panov Timofey <kirito.panov57@Gmail.com>
2022-07-23 21:42:06 +0300

    2 задание по 5 семинару

---
+++ Sem005_Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿// 1. Напишите программу замена элементов массива: положительные элементы заменить на соответствующие отрицательные и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+namespace Seminar005
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            int count = new Random().Next(5, 11);
+            int[] arrayNumber = new int[count];
+            Console.WriteLine("Первоначальный массив: ");
+            FillArray(arrayNumber);
+            Console.WriteLine();
+            Console.WriteLine("Перевернутый массив: ");
+            PrintChangeArray(arrayNumber);
+            Console.ReadKey();
+        }
+        // Заполнение и печать Массива
+        public static void FillArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int number = new Random().Next(-20, 20);
+                array[i] = number;
+                Console.Write($"{array[i]} ");
+            }
+        }
+        // Замена элементов и печать массива
+        public static void PrintChangeArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i] * -1;
+                Console.Write($"{array[i]} ");
+            }
+        }
+    }
+}
