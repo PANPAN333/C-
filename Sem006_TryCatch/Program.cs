035cf5012848135dfbb7cc09a160a6f64ec2f021
Panov Timofey <kirito.panov57@Gmail.com>
2022-07-24 09:26:32 +0300

    разбор функции TryCatch

---
+++ Sem006_TryCatch/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Seminar6
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try // (Пробовать)
+            {
+                string? str = Console.ReadLine();
+                int num = Convert.ToInt32(str);
+            }
+            catch 
+            {
+                Console.Write("Вы ввели неправильные данные");  //throw new Exception(); // throw (можно попробовать выкинут исключение самостоятельно).
+            }
+        }
+    }
+}
