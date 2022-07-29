cfa2cd5bf5f89c0642e0c5718d4ee4818a314fa3
Panov Timofey <kirito.panov57@Gmail.com>
2022-07-29 19:21:17 +0300

    Программа на замену столбцов на строки в двумерном массиве

---
+++ Sem008_Massivs/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу которая в двумерном массиве меняет местами столбцы и строки.
+
+namespace Seminar008
+{
+    class program
+    {
+        static int[,] A ={{1, 4, 7, 2},
+                          {5, 9, 2, 3},
+                          {8, 4, 2, 4},
+                          {5, 2, 6, 7}};
+        static bool CheckArrayParam(int[,] a) // проверка 
+        {
+            if(a.GetUpperBound(0) == a.GetUpperBound(1)) return true; // a.GetUpperBound(0) (rows) == a.GetUpperBound(1) (columns)
+            else return false;
+        }
+        static int[,] TransponArray(int[,] a)  // функция которая занимается транспонированием матрицы
+        {
+            int[,] b = new int[a.GetUpperBound(0) + 1, a.GetUpperBound(1) + 1];
+            for (int i = 0; i < a.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < a.GetUpperBound(1) + 1; j++)
+                {
+                    b[j, i] = a[i,j];
+                }
+            }
+            return b;
+        }                   
+        
+        static void ViewArray(int[,] a) // функция котрая выводит массив на экран
+        {
+            for (int i = 0; i < a.GetUpperBound(0) + 1; i++)
+            {
+                Console.WriteLine("");
+                for (int j = 0; j < a.GetUpperBound(1) + 1; j++)
+                {
+                    Console.Write(a[i, j]);
+                }
+            }
+            Console.WriteLine("");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Например, задан массив");
+            ViewArray(A);
+            if (CheckArrayParam(A))
+            {
+                A = TransponArray(A);
+                Console.WriteLine("В итоге получается такой массив: ");
+                ViewArray(A);
+            }
+            else
+            Console.WriteLine("Невозможно транспонировать ");
+        }    
+    }
+}
